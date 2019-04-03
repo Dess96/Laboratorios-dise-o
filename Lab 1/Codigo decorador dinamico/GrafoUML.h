1cda375fac971a18b22c9cdcf67a41ee86c2b6c1
Dess96 <dessalfaro@gmail.com>
2019-04-03 16:32:08 -0600

    feat: Se agregan las clases enriquecidas
    *Se agregan las clases faltantes: Grafo, GrafoUML, ND_UML

---
+++ Lab 1/Codigo decorador dinamico/GrafoUML.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef GRAF_UML
+#define GRAF_UML
+#include"Grafo.h"
+#include"ND_UML.h"
+
+class GrafoUML {
+private:
+	Grafo grafo;
+public:
+	void agregar_clase(ND_UML nodo_clase);
+	void agregar_paquete(ND_UML nodo_paq);
+	void agregar_objeto(ND_UML nodo_obj);
+	void eliminar_clase(int id);
+	void eliminar_paquete(int id);
+	void eliminar_objeto(int id);
+};
+#endif // !GRAF_UML
+
