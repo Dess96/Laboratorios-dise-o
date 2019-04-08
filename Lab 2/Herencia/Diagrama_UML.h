76bfd685aeafaf4439c8544462957c9c32b0d529
Dess96 <dessalfaro@gmail.com>
2019-04-07 17:24:40 -0700

    feat: Constructor y destructor agregados

--- Lab 2/Herencia/Diagrama_UML.h
+++ Lab 2/Herencia/Diagrama_UML.h
@@ -1,7 +1,9 @@
 #ifndef DIAGRA
 #define DIAGRA
 class Diagrama_UML {
-
+public:
+	Diagrama_UML();
+	~Diagrama_UML();
 };
 #endif // !DIAGRA
 
--- Lab 2/Herencia/Paquete_UML.h
+++ Lab 2/Herencia/Paquete_UML.h
@@ -1,7 +1,9 @@
 #ifndef PAQ
 #define PAQ
 class Paquete_UML {
-
+public:
+	Paquete_UML();
+	~Paquete_UML();
 };
 #endif // !PAQ
 
