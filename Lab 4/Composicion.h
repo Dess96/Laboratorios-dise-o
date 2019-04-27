1132a44d5641044c1bbf50cd4a731896b1201024
Dess96 <dessalfaro@gmail.com>
2019-04-27 16:59:57 -0600

    feat: Clases necesarias

---
+++ Lab 4/Composicion.h
@@ -0,0 +1,12 @@
+
+class Composicion : public Arista_UML {
+public:
+	Composicion() {};
+	~Composicion() {};
+	virtual void imprimir() const override {
+		Arista_UML uml;
+		uml.imprimir();
+		cout << " tiene composicion ";
+	}
+};
+
---
+++ Lab 4/Herencia.h
@@ -0,0 +1,10 @@
+class Herencia : public Arista_UML {
+public:
+	Herencia() {};
+	~Herencia() {};
+	virtual void imprimir() const override {
+		Arista_UML uml;
+		uml.imprimir();
+		cout << " tiene herencia ";
+	}
+};
